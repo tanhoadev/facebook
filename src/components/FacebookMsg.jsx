160f95070f380a9e14c7c6a077d3b18702b76e64
tanhoadev <buitanhoadev@gmail.com>
2024-06-03 21:52:06 +0700

    sss

--- src/components/FacebookMsg.jsx
+++ src/components/FacebookMsg.jsx
@@ -6,8 +6,8 @@ function FacebookMsg() {
 
     return (
         <>
-            <h1>hihihsi</h1>1115089526260261
-            <FacebookProvider appId="1115089526260261" chatSupport>
+            <h1>hihihsi</h1>1173070237209119
+            <FacebookProvider appId="1173070237209119" chatSupport>
                 <CustomChat pageId="351071262235120" minimized={true} />
             </FacebookProvider>
         </>
